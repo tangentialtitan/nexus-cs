77d3f54c5e2675ab153b6baf662a7889fb940f6f
JayShah-IIT <jshah3251@gmail.com>
2026-04-02 09:39:48 +0000

    fix missing a tag

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -214,6 +214,7 @@ export default async function DashboardPage() {
               </div>
               <div className="p-2">
                 {QUICK_LINKS.map((l) => (
+                  <a
                   
                     key={l.label}
                     href={l.href}
