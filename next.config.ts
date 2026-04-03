0dd2dcd1bbb668f75af2c947ec6fe3adafb9ceb1
AbhinavPJ <abhipj159@gmail.com>
2026-04-03 23:58:53 +0530

    done

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
       allowedOrigins: [
-        'improved-potato-jj65p7g75jjq35rqj-3000.app.github.dev',
+        'https://nexus-cs-psi.vercel.app',
         'localhost:3000',
       ],
     },
